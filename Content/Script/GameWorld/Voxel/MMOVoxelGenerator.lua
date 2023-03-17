f9ce57d548f3c2dc605e86494c1737c77a24328a
ouczbs <ouczbs@qq.com>
2023-03-17 12:59:33 +0800

    voxel mc

--- Content/Script/GameWorld/Voxel/MMOVoxelGenerator.lua
+++ Content/Script/GameWorld/Voxel/MMOVoxelGenerator.lua
@@ -8,15 +8,15 @@ end
 local r = 100
 local empty = -100
 function class:GetValue(X ,Y ,Z ,LOD)
-    -- if X > r or X < -r then 
-    --     return empty
-    -- end
-    -- if Y > r or Y < -r then 
-    --     return empty
-    -- end
-    -- if Z > r or Z < -r then 
-    --     return empty
-    -- end
+    if X > r or X < -r then 
+        return empty
+    end
+    if Y > r or Y < -r then 
+        return empty
+    end
+    if Z > r or Z < -r then 
+        return empty
+    end
     return Z + 0.001
 end
 function class:GetMaterialType(X ,Y ,Z ,LOD)
--- Content/Script/GameWorld/Voxel/VoxelWorld.lua
+++ Content/Script/GameWorld/Voxel/VoxelWorld.lua
@@ -6,6 +6,7 @@ function class:init()
    print("init VoxelWorld", UE.AVoxelWorld , UE.UMMOVoxelGenerator)
    local Generator = NewObject(UE.UMMOVoxelGenerator, gWorld:getWorldContext(), nil, gLuaObject.UMMOVoxelGenerator)
    Generator:init()
+   self.VisibleChunksCollisionsMaxLOD = 32
    self:SetGeneratorObject(Generator)
    local mat = LoadObject("/Game/Genshin/Voxel/MI_VoxelQuixel_FiveWayBlend_Inst")
    self.VoxelMaterial = mat
